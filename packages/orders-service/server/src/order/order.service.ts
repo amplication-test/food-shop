cc350bfbe6e9243340cf69952113121383b147ec
tupe12334 <ofek.gabay.he@gmail.com>
2023-03-26 17:09:51 +0300

    add notify function

--- packages/orders-service/server/src/order/order.service.ts
+++ packages/orders-service/server/src/order/order.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { Order, Prisma } from "@prisma/client";
 import { PrismaService } from "../prisma/prisma.service";
 import { OrderServiceBase } from "./base/order.service.base";
 
@@ -6,5 +7,12 @@ import { OrderServiceBase } from "./base/order.service.base";
 export class OrderService extends OrderServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
+  }
+
+  async create<T extends Prisma.OrderCreateArgs>(
+    args: Prisma.SelectSubset<T, Prisma.OrderCreateArgs>
+  ): Promise<Order> {
+    console.log("Notify here");
+    return super.create(args);
   }
 }
